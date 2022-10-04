774062ffeab777225960194bf201e666eabdfe13
jpeg-bolt <44444927+jpeg-bolt@users.noreply.github.com>
2022-10-04 23:29:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,35 @@ var logloss = ['Ship: Aman <br><br> Date: August 2017 <br><br> Location: Off the
                
                'Ship: Haj Abdullah<br><br>Date: 15 July 2021<br><br>Location: Mogadishu, Somalia<br><br>Crew: Egypt(8); Lebanon(2); Syrian Arab Republic(1)<br><br>The ship had been hit by a tornado, which cracked the hull and caused fuel to start leaking. They carried out an emergency docking in Somalia, but were refused entry as they had no visas. The ship was laden with sulfur, a highly flammable material, which reacts with salt water to produce a toxic compound that can inhibit breathing.<br><br>The owner accused the crew of sabotaging the ship, and refused to pay their wages or facilitate their repatriation. The ship’s insurer, Thomas Miller, claimed that as the ship was damaged, it was not seaworthy and therefore not covered by their policy. Left with no escape, the crew spent 8 months aboard the ship, before finally being repatriated. They have still not received their full wages.',
                
-               'Ship: Badr<br><br>Date: 1 May 2022<br><br>Location: Burgas, Bulgaria<br><br>Crew: Ukraine (15)<br><br>All 15 crew are owed 5 months of wages. The ship has been stuck to same patch of sea for 3 years now. Skeleton crews come and go, stuck at sea with intermittent food and water.  It’s flagged to Cameroon and locked in an ongoing dispute with Bulgarian port authorities. In 2020 the owner changes the name, and for unknown reasons switches the flag to Israel, another country that hasn’t ratified the Maritime Labor Convention. The crew start to receive threatening phone calls, telling them to stop organising, or they’ll be made unemployable. The insurance company they are using is called East of England P&I, a name chosen to make it sound like they’re one of the big insurance players, West of England or North of England, but it’s a mysterious company with an office in Cyprus that turns out to be uncontactable. Up the coast on the black sea, war rages.'
+               'Ship: Badr<br><br>Date: 1 May 2022<br><br>Location: Burgas, Bulgaria<br><br>Crew: Ukraine (15)<br><br>All 15 crew are owed 5 months of wages. The ship has been stuck to same patch of sea for 3 years now. Skeleton crews come and go, stuck at sea with intermittent food and water.  It’s flagged to Cameroon and locked in an ongoing dispute with Bulgarian port authorities. In 2020 the owner changes the name, and for unknown reasons switches the flag to Israel, another country that hasn’t ratified the Maritime Labor Convention. The crew start to receive threatening phone calls, telling them to stop organising, or they’ll be made unemployable. The insurance company they are using is called East of England P&I, a name chosen to make it sound like they’re one of the big insurance players, West of England or North of England, but it’s a mysterious company with an office in Cyprus that turns out to be uncontactable. Up the coast on the black sea, war rages.',
+               
+           'Haj Abdullah <br>off the coast of Dar es Salaam, Tanzania<br>25 Dec 2021 - 28 Feb 2022 <br><img src="hajabdullahtrace.png">',
+               
+               'Ying Hao I (FKA Angelic Power)<br><br>1 March - 22nd November 2020<br><br>Kuishan Island, near Hong Kong<br><img src="YingHao1Trace.png">',
+               
+               'Queen Luca<br><br>17th April - 26th November 2020<br><br>ff coast of Istanbul.<br><br>(Reported 1 June, but drifting since April. Marked resolved by ITF on 6 September, but still drifting at anchor until 26 November)<img src="QueenLucaTrace.png">',
+               
+               'Shiu Jing Ling<br><br>20 May - 20 June 2020<br><br>off the coast of Taiwan<br><img src="ShiuJingLingTrace.png">',
+               
+                   'Shiu Jing Ling<br><br>20 May - 20 June 2020<br><br>off the coast of Taiwan<br><img src="shiujinglingtrace2.png">',
+               
+               
+                  'Queen Luca<br><br>17th April - 26th November 2020<br><br>ff coast of Istanbul.<br><br>(Reported 1 June, but drifting since April. Marked resolved by ITF on 6 September, but still drifting at anchor until 26 November)<img src="queenluca2.png">',
+               
+               'Ship: Muru<br><br>Date: 27 June 2022<br><br>Location: Sharjah, UAE<br><br>Crew: Philippines (7)<br><br>The crew are out of water. They are now drinking water that’s been in the air conditioner. The employment contracts have inconsistent IMO numbers or are for ships with different names. The registered owner listed in the IMO’s database looks like a shell company structure.',
+               
+               'Ship: Bianca<br><br>Date: 23 June 2022<br><br>Location: Dakar, Senegal<br><br>Crew: Gambia; Guinea-Bissau; Russian Federation; Ukraine<br><br>It’s been seven months since they were last paid. For unknown reasons, the owner changes the ship’s name. The registered insurer writes to the ILO, denying any association with the ship and asking to be removed from the database. The German state also writes in to deny any association. The crew are owed $222,000 dollars. Someone at the insurance firm, after negating any liability, expresses that they hope the crew can be repatriated ‘as soon as practicable’.',
+               
+               'Ship: Princess Miral<br><br>Date: 21 June 2022<br><br>Location: New Mangalore, India<br><br>Crew: Syrian Arab Republic(15)<br><br>After the boat takes on seawater and sinks, the crew are rescued by coast guard search and rescue teams. They are then taken to a detention facility. The ship operator tries to make the crew pay fake wage receipts, in return for their repatriation home.',
+               
+               'Ship: Eoilika<br><br>Date: 4 April 2022<br><br>Location: Dakar, Senegal<br><br>Crew: India(2); Ukraine(2)<br><br>Some of the crew are being held in prison. The ITF contact the flag state, owners, and insurance company, but get no reply or even an acknowledgement of their messages. The ship’s registered insurer is East of England P&I, a name chosen to sound like the group of English P&I (protection and indemnity) clubs that cover 90% of the world fleet. However, the company is registered in Seychelles and managed in Cyprus, with an uncontactable phone number. 3 other ships that have been abandoned in the last 2 years are also allegedly registered with the insurer. The ITF eventually repatriate the crew themselves on humanitarian grounds.',
+               
+        'Ship: PSD 2<br><br>Date: 17 January 2022<br><br>Location: Durban, South Africa<br><br>Crew: India (2)<br><br>The ship has been there for years. The last crew got off, after this 2 more seafarers were sent to watch the ship. They are not being paid. They have no electricity and no bathroom. The owner offers 2 months of wages, far below what they are owed. Against the advice of the ITF, the crew accept it, fearing that their families can not get by without them. As they leave, the owner sends another crew to watch the ship.',
+          
+               'Ship: Project Trader<br><br>Date: 7 January 2022<br><br>Location: Al Hamriya Port, UAE<br><br>Crew: India(9); Pakistan(2); Tanzania(1)<br><br>The crew are owed 9 months of wages. Some of the them are accepting 2 months wages and going home. Some are accepting 1 month with the promise of 2 more, upon transfer to another ship. Not far away, in Jebel Ali, the owner has abandoned another ship. The captain is standing his ground, refusing any offer less than what he owes. The government of Comoros, who get tax proceeds from the owner registering the ship there, are trying to lobby the ILO on the owner’s behalf, calling the seafarer’s descriptions of their predicament ‘totally baseless’.'
+               
+               
+
                 
                  ];
 
@@ -36,7 +64,3 @@ function concept(){
     
 
 }
-
-
-
-
