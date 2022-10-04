d995ca07d60076cc4c283c4a9b9adb581764905b
jpeg-bolt <44444927+jpeg-bolt@users.noreply.github.com>
2022-10-04 16:21:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+var logloss = ['Ship: Aman <br><br> Date: August 2017 <br><br> Location: Off the coast of Egypt <br><br> Crew: Egypt(9); India(3); Syria(4) <br><br> Mohammad Aisha’s mother passes away while he is stuck on an abandoned ship. He has already been there for one year. The ship was impounded due to lack of safety equipment and the owner is not helping out. As the legal guardian, new recruit Mohammad must stay aboard.', 
+               
+               'Ship: MV Jinan<br><br>Date: 9 April 2021<br><br>Location: Port of Mombasa, Kenya<br><br>Crew: Syrian Arab Republic(18)<br><br>The power cuts out, shutting off the lights and the internet.  Several conversations with families back home in Syria cut off sharply. Electricity is being rationed — everything is being rationed. In a few days Reverend Muli from the mission to seafarers will row out to the ship with flour, cooking oil, vegetables from the market, fuel for lights and internet. He can row out to them but they cannot come ashore to visit him. They hope that this time he will also bring cigarettes.',
+               
+               'Ship: Ocean Force<br><br>Date: 29 January 2022<br><br>Location: Philadelphia, USA<br><br>Crew: Kazakhstan(1); Ukraine(6)<br><br>The ship has been under arrest for 11 months, wages and repatriation efforts have been slow. They have had no fuel to power the heating for four days. It is minus 8 degrees celsius outside. 6 of the 7 crew are Ukrainian, and in early February they are instructed to go get their wages in cash from Odessa. 2 weeks later, war breaks out.',
+               
+            'Ship: Hilmi K<br><br>Date: 29 November 2021<br><br>Location: Martas, Turkey<br><br>Crew: Pakistan(1); Philippines(9),br>The ship is anchored near the port of Martas, but the winds are so strong that it drags the anchor and severs it. The Hurricane blows the ship 24km away and destroys most of the ships systems. The master of the ship, who under maritime law is financially responsible for the damage, moves to protect himself: he accuses the crew of sabotage.',
+               
+               'Ship: Sea Queen<br><br>Date: 8th December 2021<br><br>Location: Abu Qir, Egypt<br><br>Crew: Egypt(6); India(6); Syrian Arab Republic(2)<br><br>A crew member abandons the sinking ship and swims all night until he reaches an offshore oil rig. Of the 14 crew, 11 survive. Some of them had been on the ship for 17 months, unpaid. The insurance certificate that they recover from the wreck turns out to be fake.',
+               
+               'Ship: Monarch Princess<br><br>Date: 11 April 2022<br><br>Location: Fort Pierce, Florida<br><br>Crew: Panama (4), Peru (1)<br><br>Food has almost run out. The owner of the ship is not responding. Last year the owner of the ship abandoned another, in similar circumstances. 200m away, on the seafront, there is a seafood restaurant, a tiki bar, a diner, but they cannot go ashore.',
+                
+                 'Ship: Jian Ye<br><br>Date: 28 March 2022<br><br>Location: Koahsiung, Taiwan<br><br>Crew: China (1), Indonesia, (8).<br><br>The crew have not been paid for two months. The ship is registered in Togo, but the Togolese government deny this, claiming that the paperwork is fake. According to the government of China, the company that own the ship, Jianfu Shipping co. Ltd, no longer exists. The crew just want to get home.',
+               
+               'Ship: HL HANNA<br><br>Location: Bandar Abbas, Iran<br><br>Date: 1 March 2022<br><br>Crew: Iran, 16.<br><br>The crew are in their home country, but they remain on the boat in the hopes of eventually getting paid. Together they are owed around $384,000 dollars. Some of them have been there for over 16 months. Many haven’t received any payment in over a year. They only have enough fuel to run the engine for 2 hours a day: outside of this its blackout, no electricity, no internet, no lights, no cooling. Temperatures get to around 40 degrees, but in the metal box of the boat it feels hotter.',
+               
+               'Ship: Monarch Countess<br><br>Location: Gonviales, Haiti<br><br>Date: 18 November 2021<br><br>Crew: Unknown<br><br>A worker dies on board a cruise ship, in unknown circumstances. For two months after the crew remain on the ship. Food supplies have been dwindling. They receive no wages. The master of the ship assaults the third engineer, and he is taken off the ship for hospitalisation. One year later and Monarch, the company, are still operating. They abandon another cruise ship, the Monarch Princess.',
+               
+               'Ship: Saoshyant<br><br>Location: Busan, Korea<br><br>Date: 24 February 2022<br><br>Crew: Iran (11), Nigeria (5), India (8).<br><br>For some time the crew have been paid less than the International Labor Organisation minimum wage of $23 a day. For the past two months, they’ve received no salary at all. The same day they officially declare themselves abandoned, Russia invades Ukraine. The ILO are attempting to make the insurance company pick up the unpaid wages. The seafarers want to get home, apparently its not possible via Chinese ports. Eventually they are repatriated from Vladivostok, Russia, two months into the invasion.',
+               
+               'Ship: Captain Nagdaliyev<br><br>Date: 13 March 2020<br><br>Location: Beirut, Lebanon<br><br>Crew: Azerbaijan (10), Russia (2), Turkey (1)<br><br>The pandemic hits and the owner stops picking up the phone. The crew are left in beirut port. In July the fresh water runs out. In August a massive explosion destroys the port. The stray ammonium nitrate that sparked the blast came from another abandoned ship, 7 years before. In October they are still stranded, still running low on everything. The case goes to court. The russian crew get reaptriated home, the Azerbaijanis don’t. In February 2021, after one year on board, they go on hunger strike. Two weeks later they are repatriated. The ship’s insurance pays out for four months of wages. They are still trying to get the rest.',
+               
+               'Ship: Haj Abdullah<br><br>Date: 15 July 2021<br><br>Location: Mogadishu, Somalia<br><br>Crew: Egypt(8); Lebanon(2); Syrian Arab Republic(1)<br><br>The ship had been hit by a tornado, which cracked the hull and caused fuel to start leaking. They carried out an emergency docking in Somalia, but were refused entry as they had no visas. The ship was laden with sulfur, a highly flammable material, which reacts with salt water to produce a toxic compound that can inhibit breathing.<br><br>The owner accused the crew of sabotaging the ship, and refused to pay their wages or facilitate their repatriation. The ship’s insurer, Thomas Miller, claimed that as the ship was damaged, it was not seaworthy and therefore not covered by their policy. Left with no escape, the crew spent 8 months aboard the ship, before finally being repatriated. They have still not received their full wages.',
+               
+               'Ship: Badr<br><br>Date: 1 May 2022<br><br>Location: Burgas, Bulgaria<br><br>Crew: Ukraine (15)<br><br>All 15 crew are owed 5 months of wages. The ship has been stuck to same patch of sea for 3 years now. Skeleton crews come and go, stuck at sea with intermittent food and water.  It’s flagged to Cameroon and locked in an ongoing dispute with Bulgarian port authorities. In 2020 the owner changes the name, and for unknown reasons switches the flag to Israel, another country that hasn’t ratified the Maritime Labor Convention. The crew start to receive threatening phone calls, telling them to stop organising, or they’ll be made unemployable. The insurance company they are using is called East of England P&I, a name chosen to make it sound like they’re one of the big insurance players, West of England or North of England, but it’s a mysterious company with an office in Cyprus that turns out to be uncontactable. Up the coast on the black sea, war rages.'
+                
+                 ];
+
+
+
+
+function concept(){
+    var randomNumber = Math.floor(Math.random() * (logloss.length));
+   
+    document.getElementById('conceptHere').innerHTML = (logloss[randomNumber]);;
+    
+
+}
+
+
+
+
